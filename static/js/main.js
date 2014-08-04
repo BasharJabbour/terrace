27072951d01f080995fd5f0ec37128c2d7519c41
Justin Duke <me@jmduke.com>
2014-08-03 20:49:01 -0700

    Integrate lifestream.

--- static/js/main.js
+++ static/js/main.js
@@ -17,6 +17,73 @@ $(document).ready(function() {
         $('pre').toggle();
         $('.code').html($('.code').html() == "(Hiding code.  Click to show.)" ? "(Showing code.  Click to hide.)" : "(Hiding code.  Click to show.)");
     });
+
+    (function(){
+      var count = 0,
+      list = [
+        {
+          service: 'reddit',
+          user: 'jmduke'
+        },
+        {
+          service: 'github',
+          user: 'jmduke'
+        },
+        {
+          service: 'hypem',
+          user: 'jmduke'
+        },
+        {
+          service: 'tumblr',
+          user: 'jmduke'
+        },
+        {
+          service: 'twitter',
+          user: 'justinmduke'
+        },
+        {
+          service: 'stackoverflow',
+          user: '3344238'
+        }
+      ];
+
+      Date.prototype.toISO8601 = function(date) {
+          var pad = function (amount, width) {
+              var padding = "";
+              while (padding.length < width - 1 && amount < Math.pow(10, width - padding.length - 1))
+                  padding += "0";
+              return padding + amount.toString();
+          }
+          date = date ? date : new Date();
+          var offset = date.getTimezoneOffset();
+          return pad(date.getFullYear(), 4)
+              + "-" + pad(date.getMonth() + 1, 2)
+              + "-" + pad(date.getDate(), 2)
+              + "T" + pad(date.getHours(), 2)
+              + ":" + pad(date.getMinutes(), 2)
+              + ":" + pad(date.getUTCSeconds(), 2)
+              + (offset > 0 ? "-" : "+")
+              + pad(Math.floor(Math.abs(offset) / 60), 2)
+              + ":" + pad(Math.abs(offset) % 60, 2);
+      };
+
+      $("#lifestream div").lifestream({
+        limit: 30,
+        list: list,
+        feedloaded: function(){
+          count++;
+          // Check if all the feeds have been loaded
+          if( count === list.length + 1 ){
+            $("#lifestream li").each(function(){
+              var element = $(this),
+                  date = new Date(element.data("time"));
+              element.append(' <abbr class="timeago" title="' + date.toISO8601(date) + '">' + date + "</abbr>");
+            })
+            $("#lifestream .timeago").timeago();
+          }
+        }
+      });
+    })();
 });
 
 $(window).on('scroll', function() {
